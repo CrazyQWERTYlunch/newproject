94d9a4a6c5ddf41bc9b61b7ac57e68ea3373afed
UzNik <uznik.99@mail.ru>
2024-01-30 21:23:59 +0300

    Added homework for the 5 seminar

--- homeworks_app5/admin.py
+++ homeworks_app5/admin.py
@@ -1,64 +1,61 @@
 from django.contrib import admin
-#
-# from homeworks_app3.models import Client, Product, OrderItem, Order
-#
-#
-# @admin.register(Client)
-# class ClientAdmin(admin.ModelAdmin):
-#     list_display = ['username', 'email', ]
-#     search_fields = ['username', 'email', ]
-#
-#     # inlines = [CartTabAdmin, OrderTabulareAdmin,]
-#
-#
-# @admin.register(Product)
-# class ProductsAdmin(admin.ModelAdmin):
-#     prepopulated_fields = {'slug': ('name',)}
-#     list_display = ['name', 'quantity', 'price', 'discount']
-#     list_editable = ['discount', ]
-#     search_fields = ['name', 'description']
-#     list_filter = ['discount', 'quantity', ]
-#     fields = [
-#         "name",
-#         "slug",
-#         "description",
-#         # "image",
-#         ("price"),
-#         "quantity",
-#     ]
-#
-# @admin.register(OrderItem)
-# class OrderItemAdmin(admin.ModelAdmin):
-#     list_display = "order", "product", "price", "quantity"
-#     search_fields = (
-#         "order",
-#         "product",
-#     )
-#
-#
-# @admin.register(Order)
-# class OrderAdmin(admin.ModelAdmin):
-#     list_display = (
-#         "id",
-#         "client",
-#         "requires_delivery",
-#         "status",
-#         "payment_on_get",
-#         "is_paid",
-#         "created_timestamp",
-#     )
-#     search_fields = (
-#         "id",
-#     )
-#
-#     # readonly_fields = ("created_timestamp",)
-#
-#     list_filter = (
-#         "requires_delivery",
-#         "status",
-#         "payment_on_get",
-#         "is_paid",
-#         "created_timestamp",
-#     )
-#
-#     # inlines = (OrderItemTabulareAdmin,)
+
+from homeworks_app3.models import Client, Product, OrderItem, Order
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email', 'phone_number']
+    search_fields = ['username', 'email', ]
+
+
+
+@admin.register(Product)
+class ProductsAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+    list_display = ['name', 'quantity', 'price', 'image']
+    list_editable = ['price']
+    search_fields = ['name', 'description']
+    list_filter = ['quantity', 'price']
+    fields = [
+        "name",
+        "slug",
+        "description",
+        "image",
+        "price",
+        "quantity",
+    ]
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = "order", "product", "quantity"
+    search_fields = (
+        "order",
+        "product",
+    )
+    readonly_fields = ('created_timestamp',)
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "client",
+        "requires_delivery",
+        "status",
+        "payment_on_get",
+        "is_paid",
+    )
+    search_fields = (
+        "id", "client"
+    )
+    readonly_fields = ("created_timestamp",)
+
+    list_filter = (
+        "requires_delivery",
+        "status",
+        "payment_on_get",
+        "is_paid",
+        "created_timestamp",
+    )
